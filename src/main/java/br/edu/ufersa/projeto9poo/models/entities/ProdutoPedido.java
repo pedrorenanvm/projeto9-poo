0b71647d9ef5043f7a486c8956056aa9c270b088
Pedro Renan <pedroviana15094@gmail.com>
2025-06-23 23:55:48 -0300

    feat: Refatorar entidade ProdutoPedido

--- src/main/java/br/edu/ufersa/projeto9poo/models/entities/ProdutoPedido.java
+++ src/main/java/br/edu/ufersa/projeto9poo/models/entities/ProdutoPedido.java
@@ -1,52 +1,102 @@
 package br.edu.ufersa.projeto9poo.models.entities;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name= "produto_pedido")
+
 public class ProdutoPedido{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional=false)
+    @JoinColumn(name ="produto_id")
     private Produto produto;
-    private Adicional adicional[];
+
+    @ManyToMany
+    @JoinTable(
+            name = "produto_pedido_adicional",
+            joinColumns = @JoinColumn(name = "produto_pedido_id"),
+            inverseJoinColumns = @JoinColumn(name = "adicional_id")
+    )
+    private List<Adicional> adicionais;
+
+    @ManyToOne(optional=false)
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+
+    @Column(nullable = false)
     private int quantidade;
+
+    @Column(nullable = false)
     private long precoUnidade;
 
-    public ProdutoPedido(Produto produto, Adicional adicional[],int quantidade, long precoUnidade){
+    public ProdutoPedido(){}
+
+    public ProdutoPedido(Produto produto, List<Adicional> adicionais, int quantidade, long precoUnidade){
         this.produto = produto;
-        this.adicional = adicional;
+        this.adicionais = adicionais;
         setQuantidade(quantidade);
         setPrecoUnidade(precoUnidade);
     }
-
+    // TODO: Verificar se o método deve ser assim mesmo
     public long precoTotal(){
         return this.quantidade*this.precoUnidade;
     }
 
-    public void setQuantidade(int q){
-        if (q>0){
-            quantidade = q;
-        } else{
-            System.out.println("Diga uma quantidade maior que 0");
-        }
+    public Long getId() {
+        return id;
     }
-    public void setPrecoUnidade(long pU){
-        if (pU>0){
-            precoUnidade = pU;
-        } else{
-            System.out.println("Diga uma quantidade maior que 0");
-        }
-    }
-    public int getQuantidade(){
-        return this.quantidade;
-    }
-    public long getprecoUnidade(){
-        return this.precoUnidade;
-    }
-    public void setProduto(Produto produto){
-        this.produto = produto;
-    }
-    public Produto getProduto(){
+
+    public Produto getProduto() {
         return produto;
     }
-    public void setAdicional(Adicional[] adicional){
-        this.adicional = adicional;
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
     }
-    public Adicional[] getAdicional(){
-        return this.adicional;
+
+    public List<Adicional> getAdicionais() {
+        return adicionais;
     }
+
+    public void setAdicionais(List<Adicional> adicionais) {
+        this.adicionais = adicionais;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        if (quantidade > 0) {
+            this.quantidade = quantidade;
+        } else {
+            throw new IllegalArgumentException("A quantidade deve ser maior que zero.");
+        }
+    }
+
+    public long getPrecoUnidade() {
+        return precoUnidade;
+    }
+
+    public void setPrecoUnidade(long precoUnidade) {
+        if (precoUnidade > 0) {
+            this.precoUnidade = precoUnidade;
+        } else {
+            throw new IllegalArgumentException("O preço unitário deve ser maior que zero.");
+        }
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
 }
