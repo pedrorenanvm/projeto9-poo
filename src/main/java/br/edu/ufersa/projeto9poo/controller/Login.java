81504d7ae48aec1a08b0e528581538eb048ebb45
Pedro Renan <pedroviana15094@gmail.com>
2025-07-29 15:32:39 -0300

    feat: adicionar diretorio assets com imagens e font do projeto

---
+++ src/main/java/br/edu/ufersa/projeto9poo/controller/Login.java
@@ -0,0 +1,57 @@
+package br.edu.ufersa.projeto9poo.controller;
+
+import br.edu.ufersa.projeto9poo.models.entities.Funcionario;
+import br.edu.ufersa.projeto9poo.models.services.FuncionarioService;
+import br.edu.ufersa.projeto9poo.models.services.FuncionarioServiceImpl;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Login {
+
+    @FXML
+    private TextField txtUsuario;
+
+    @FXML
+    private PasswordField txtSenha;
+
+    @FXML
+    private Button btnLogin;
+
+    private final FuncionarioService funcionarioService = new FuncionarioServiceImpl();
+
+
+    @FXML
+    private void handleLogin() {
+
+        String senha = txtSenha.getText();
+        String usuario = txtUsuario.getText();
+
+        try{
+            Funcionario funcionario = new Funcionario(usuario, senha);
+            Funcionario logado = funcionarioService.logar(funcionario);
+
+            // Carregar a próxima tela aqui
+        } catch(IllegalArgumentException e){
+            mostrarAlertaErro("Login inválido", e.getMessage());
+
+        }catch (Exception e){
+            mostrarAlertaErro("Error inesperado", "Ocorreu um erro ao efetuar login");
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private void mostrarAlertaErro(String titulo, String mensagem) {
+        Alert alerta = new Alert(Alert.AlertType.ERROR);
+        alerta.setTitle(titulo);
+        alerta.setHeaderText(null);
+        alerta.setContentText(mensagem);
+        alerta.showAndWait();
+    }
+
+}
+
