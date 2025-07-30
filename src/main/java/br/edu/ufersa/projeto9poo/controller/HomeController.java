70e2698530f8768649ea47cc5a58326b7765f980
elnatanorato <elnatan.honorato@gmail.com>
2025-07-30 09:37:37 -0300

    Adicionando funcionalidade do home

--- src/main/java/br/edu/ufersa/projeto9poo/controller/HomeController.java
+++ src/main/java/br/edu/ufersa/projeto9poo/controller/HomeController.java
@@ -1,4 +1,74 @@
 package br.edu.ufersa.projeto9poo.controller;
 
+import br.edu.ufersa.projeto9poo.models.entities.Carrinho;
+import br.edu.ufersa.projeto9poo.models.entities.Item;
+import br.edu.ufersa.projeto9poo.models.entities.TipoEstado;
+import br.edu.ufersa.projeto9poo.models.services.CarrinhoService;
+import br.edu.ufersa.projeto9poo.models.services.CarrinhoServiceImpl;
+import br.edu.ufersa.projeto9poo.models.services.ItemCoodinatorService;
+import br.edu.ufersa.projeto9poo.models.services.ItemCoordinatorServiceImpl;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class HomeController {
+    @FXML
+    private Label labelValor;
+    @FXML
+    private Label labelItem;
+    @FXML
+    private Label labelPedido;
+    @FXML
+    private TableView<Carrinho> table;
+    @FXML
+    private TableColumn<Carrinho, Long> columnId;
+    @FXML
+    private TableColumn<Carrinho, String> columnCliente;
+    @FXML
+    private TableColumn<Carrinho, Long> columnValor;
+    @FXML
+    private TableColumn<Carrinho, String> columnStatus;
+
+    private CarrinhoService carrinhoService = new CarrinhoServiceImpl();
+    private ItemCoodinatorService itemCoodinatorService = new ItemCoordinatorServiceImpl();
+
+    @FXML
+    private void initialize() {
+        columnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        columnCliente.setCellValueFactory(p -> {
+            Carrinho carrinho = p.getValue();
+            return new ReadOnlyStringWrapper(carrinho.getCliente().getNome());
+        });
+        columnValor.setCellValueFactory(p -> {
+            Carrinho carrinho = p.getValue();
+            return new ReadOnlyObjectWrapper<Long>(carrinho.precoTotal());
+        });
+        columnStatus.setCellValueFactory(new PropertyValueFactory<>("estado"));
+
+        carregarDados();
+    }
+
+    private void carregarDados() {
+        LocalDate hoje = LocalDate.now();
+        List<Carrinho> carrinhos = carrinhoService.buscarTodos();
+        List<Carrinho> carrinhosHoje = carrinhos.stream()
+                .filter(c -> c.getData().equals(hoje))
+                .toList();
+
+        labelValor.setText(String.valueOf(carrinhosHoje.stream().map(Carrinho::precoTotal).reduce(0L, Long::sum)));
+        labelItem.setText(String.valueOf(itemCoodinatorService.buscarTodos().stream().filter(Item::isEstoque).count()));
+        labelPedido.setText(String.valueOf(carrinhos.stream().filter(p -> p.getEstado().equals(TipoEstado.EM_ANDAMENTO)).count()));
+
+        ObservableList<Carrinho> observableCarrinhosHoje = FXCollections.observableList(carrinhosHoje);
+        table.setItems(observableCarrinhosHoje);
+    }
 }
